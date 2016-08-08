8e5851cd8cdba9185c1b365e4f3c8708bdd41650
marco <marco@webguerilla.net>
2016-08-08 02:23:48 +0200

    first release

---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var htmlTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Presentation Notes</title>
+	</head>
+	<body>
+		<table>
+			{{ range . }}
+			<tr>
+				<td width="50%"><img src="{{ . }}" /></td>
+				<td></td>
+			</tr>
+			{{ end }}
+		<table>
+	</body>
+</html>
+`
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+
+	checkBinaries()
+
+	filenameIn := selectFile()
+	filenameOut := strings.Replace(filenameIn, ".pdf", ".odt", -1) // TODO use regexp to make sure that at the end
+
+	overwriteExistingFile(filenameOut)
+
+	tmpPath := fmt.Sprintf("%s/%s-%d", os.TempDir(), filenameIn, time.Now().Unix())
+	htmlFilePath := fmt.Sprintf("%s/index.html", tmpPath)
+
+	imagesDir := "images"
+	imagesPath := fmt.Sprintf("%s/%s", tmpPath, imagesDir)
+
+	convertPDFToImages(filenameIn, imagesPath)
+	createHTMLFile(htmlFilePath, imagesPath, imagesDir)
+
+	createODTDocument(filenameOut, htmlFilePath)
+
+	// TODO delete tmpPath (first echo)
+}
+
+func createODTDocument(filenameOut, htmlFilePath string) {
+	command := exec.Command("pandoc", "-f", "html", "-t", "odt", "-o", filenameOut, htmlFilePath) // TODO are the params escaped by Command?
+	if err := command.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func createHTMLFile(htmlFilePath, imagesPath, imagesDir string) {
+	t, err := template.New("notes").Parse(htmlTemplate)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	files, err := ioutil.ReadDir(imagesPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filenames := []string{}
+
+	for _, file := range files {
+		filenames = append(filenames, fmt.Sprintf("%s/%s", imagesDir, file.Name()))
+	}
+
+	file, err := os.Create(htmlFilePath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	if err := t.Execute(file, filenames); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkBinaries() {
+	if _, err := exec.LookPath("convert"); err != nil {
+		log.Fatalln("convert not installed")
+	}
+
+	if _, err := exec.LookPath("pandoc"); err != nil {
+		log.Fatalln("pandoc not installed")
+	}
+}
+
+func convertPDFToImages(filenameIn, imagesPath string) {
+	if err := os.MkdirAll(imagesPath, 0700); err != nil {
+		log.Fatalln(err)
+	}
+
+	command := exec.Command("convert", filenameIn, imagesPath+"/%003d.jpg") // TODO are the params escaped by Command?
+	if err := command.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func selectFile() string {
+	fmt.Println("Available pdf files:")
+
+	filenames, err := filepath.Glob("*.pdf")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if len(filenames) < 1 {
+		fmt.Println("No files available.")
+		os.Exit(1)
+	}
+
+	for index, filename := range filenames {
+		fmt.Printf("[%d] %s\n", index, filename)
+	}
+
+	fmt.Print("\nSelect a file: ")
+
+	var filenumber int
+	count, err := fmt.Scanf("%d", &filenumber)
+	if err != nil || count != 1 || filenumber > len(filenames) {
+		fmt.Println("Invalid input. Please try again.")
+		os.Exit(1)
+	}
+
+	return filenames[filenumber]
+}
+
+func overwriteExistingFile(filename string) {
+	_, err := os.Stat(filename)
+	if err == nil {
+		fmt.Printf("\nFile %s already exists. Do you want to overwrite the file? [yes/no]\n", filename)
+
+		var overwrite string
+		_, err := fmt.Scanf("%s", &overwrite)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if overwrite != "yes" {
+			os.Exit(0)
+		}
+	}
+
+	return
+}
